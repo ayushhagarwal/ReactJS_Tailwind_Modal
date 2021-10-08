ffa74ef8438e6162ca79f400a0fce1dd3e99e893
ayushhagarwal <ayushgrwl365@gmail.com>
2021-10-08 19:37:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-2xl text-center">
+        Hello from react and tailwind project
+      </h1>
     </div>
   );
-}
+};
 
 export default App;
