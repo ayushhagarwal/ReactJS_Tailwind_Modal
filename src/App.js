ea8d12549d97b5b0cdc954571655322e320abc69
ayushhagarwal <ayushgrwl365@gmail.com>
2022-02-06 17:49:50 +0530

    Page title Change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Modal from "./Components/Modal";
 const App = () => {
   return (
-    <div className="App h-screen flex flex-col items-center justify-center bg-gray-200">
+    <div className="App h-screen flex flex-col items-center justify-center bg-purple-200">
       <Modal />
     </div>
   );
